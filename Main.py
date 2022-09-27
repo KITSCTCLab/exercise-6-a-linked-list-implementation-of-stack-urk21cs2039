4f2ae06071e18d5935d596935d8c54a8f8c6f1fd
urk21cs2039 <110082561+urk21cs2039@users.noreply.github.com>
2022-09-27 22:36:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,57 @@
 class Node:
-def __init__(self, data):
-self.data = data
-self.next = None
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+#     self.top=None
+
 class Stack:
-def __init__(self):
-self.head = None
-def push(self, data) -> None:
-nn = Node(data)
-nn.next = self.head
-self.head = nn
-def pop(self) -> None:
-if(self.head!=None):
-temp = self.head
-self.head = temp.next
-def status(self):
-if(self.head != None):
-temp = self.head
-while temp.next!= None:
-print(temp.data, end = "")
-print("=>", end = "")
-temp = temp.next
-print(temp.data, end = "=>")
-print("None")
+  def __init__(self):
+    self.top = None
+    #self.top=None
+  def push(self, data) -> None:
+    # Write your code here
+    new=Node(data)
+    new.data=data
+    new.next=self.top
+    self.top=new
+  def pop(self) -> None:
+    # Write your code here
+    if self.top==None:
+      return None
+    else:
+      #temp=Node(data)
+      temp=self.top
+      self.top=self.top.next
+      temp.next=None
+      return temp
+      
+      
+  def status(self):
+    """
+    It prints all the elements of stack.
+    """
+    # Write your code here  
+    if self.top==None:
+      print("None")
+    else:
+      #ptr=Node(data)
+      ptr=self.top
+      while(ptr!=None):
+        print(ptr.data,end="")
+        print("=>",end="")
+        ptr=ptr.next
+        if(ptr==None):
+          print("None")
 # Do not change the following code
 stack = Stack()
 operations = []
 for specific_operation in input().split(','):
-operations.append(specific_operation.strip())
+    operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-if operations[i] == "push":
-stack.push(int(data[i]))
-elif operations[i] == "pop":
-stack.pop()
+  if operations[i] == "push":
+    stack.push(int(data[i]))
+  elif operations[i] == "pop":
+    stack.pop()
 stack.status()
